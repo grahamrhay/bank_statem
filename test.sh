9edb68f2808daa4333e93a0165a626cc00f1bb5d
Graham Hay <grahamrhay@gmail.com>
2018-04-07 15:13:07 +0100

    Add test file

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+docker run --name bank_statem -it --rm -v ${PWD}:/app -w /app -e REBAR_CACHE_DIR=/app erlang ./rebar3 eunit
---
+++ test/bank_statem_tests.erl
@@ -0,0 +1,6 @@
+-module(bank_statem_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+a_test() ->
+    ?assertEqual(true, true).
